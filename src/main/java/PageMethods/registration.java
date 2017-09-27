85ecea0fbd6cb1b11a773e43e679632e52afddd9
Ankita172 <you@example.com>
2017-09-27 10:14:38 +0530

    Latest Code

--- src/main/java/PageMethods/registration.java
+++ src/main/java/PageMethods/registration.java
@@ -2,16 +2,47 @@ package PageMethods;
 
 import java.io.IOException;
 
+
+
 import CommonMethods.CommonMethods;
 
-public class registration {
-
+public class registration extends CommonMethods {
+	
 	public static void regisrationFunctionality() throws IOException{
+		registration reg = new registration ();
 		
-		CommonMethods.waitForElement(10);
-		CommonMethods.getElement("xpath", CommonMethods.getTestProperty("firstNameElement"));
-		CommonMethods.typeAndEnter(CommonMethods.getTestProperty("firstName"));
-		CommonMethods.getElement("xpath", CommonMethods.getTestProperty("lastNameElement"));
-		CommonMethods.typeAndEnter(CommonMethods.getTestProperty("lastName"));
+		waitForElement(10);
+		
+		getElement("xpath", CommonMethods.getTestProperty("firstNameElement"));
+		typeAndEnter(CommonMethods.getTestProperty("firstName"));
+		getElement("xpath", CommonMethods.getTestProperty("lastNameElement"));
+		typeAndEnter(CommonMethods.getTestProperty("lastName"));
+		
+		getElement("xpath", getTestProperty("GenderIdFemale"));
+		click();
+		getElement("xpath", getTestProperty("YearOfExp"));
+		click();
+		scrolldown();
+		getElement("xpath", getTestProperty("DatePicker"));
+		typeAndEnter(getTestProperty("ValueOfDate"));
+		getElement("xpath", getTestProperty("ProfessionCheckbox"));
+		click();
+		getElement("xpath", getTestProperty("AutomationToolCheckbox"));
+		click();
+		getElement("xpath", getTestProperty("Continents"));
+		typeAndEnter(getTestProperty("SelectContinents"));
+		click();
+		getElement("id", getTestProperty("CommandsControl"));
+		typeAndEnter(getTestProperty("ByVisibleText"));
+		click();
+		getElement("xpath", getTestProperty("SubmitButton"));
+		click();
+		
+
+		
+		
+		
+		
+
 	}
 }
